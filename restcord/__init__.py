6c5bda7a9c875334443604a818d4429b50dc0a73
Lethys <yandawl@gmail.com>
2020-07-20 00:56:18 +0100

    Update version

--- restcord/__init__.py
+++ restcord/__init__.py
@@ -2,7 +2,7 @@ __title__ = 'restcord'
 __author__ = 'Lethys'
 __license__ = 'MIT'
 __copyright__ = 'Copyright 2020 (c) Lethys'
-__version__ = '0.0.2'
+__version__ = '0.0.3'
 
 from .client import RestCord
 from .errors import Forbidden, HTTPException, NotFound
